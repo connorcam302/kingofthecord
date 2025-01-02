4e9e89bee2a2e95d2b14ea4bfb2dc35e0a32fb25
Connor Campbell <connorcam123@gmail.com>
2025-01-02 23:19:34 +0000

    map parsing

--- src/routes/api/upload/+server.ts
+++ src/routes/api/upload/+server.ts
@@ -1,25 +1,25 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
-import { parseEvent, parseTicks } from '@laihoe/demoparser2';
+import { listGameEvents, parseEvent, parseEvents, parseTicks } from '@laihoe/demoparser2';
 
 export const POST: RequestHandler = async ({ request }) => {
 
-	const headers = {
-		'Access-Control-Allow-Origin': '*',  // Allow any origin, or set to a specific origin
-		'Access-Control-Allow-Methods': 'POST', // Allowed HTTP methods
-		'Access-Control-Allow-Headers': 'Content-Type', // Allowed headers
-	};
+	const filePath = "/home/connor/kingofthecord/src/lib/demos/003727523440987472050_2055487250.dem"
 
-	const formData = await request.formData();
-	const file = formData.get('file');
+	const gameEndTick = Math.max(...parseEvent(filePath, "round_end").map(x => x.tick))
 
-	if (!file) {
-		return json({ error: 'No file uploaded' }, { status: 400 });
-	}
+	const fields = ["kills_total", "deaths_total", "mvps", "headshot_kills_total", "ace_rounds_total", "score", "assists_total", "alive_time_total", "4k_rounds_total", "3k_rounds_total"]
+	const scoreboard = parseTicks(filePath, fields, [gameEndTick])
 
-	//@ts-ignore
-	let event_json = parseEvent(file, "player_death", ["X", "Y"], ["total_rounds_played"])
-	//@ts-ignore
-	let ticks_json = parseTicks(file, ["X", "Y"])
+	const eventNames = listGameEvents(filePath)
 
-	return json({ event_json, ticks_json }, { headers });
+	console.log(eventNames)
+
+	// Currently the event "all" gives you all events. Cursed solution for now
+	const allEvents = parseEvents(filePath, ['player_hurt', 'player_death'])
+
+	const roundTimers = parseEvents(filePath, ['round_prestart'])
+
+	return json(roundTimers)
+
+	//return json(scoreboard)
 };

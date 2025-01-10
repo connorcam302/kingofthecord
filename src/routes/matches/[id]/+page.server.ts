145283c8c572f5dbe73b8e0c538aa29fcb9b1d76
Connor Campbell <connorcam123@gmail.com>
2025-01-10 22:20:33 +0000

    added hltv rating to games

--- src/routes/matches/[id]/+page.server.ts
+++ src/routes/matches/[id]/+page.server.ts
@@ -1,8 +1,26 @@
+import { calculateADR, calculateHLTVRating, calculateImpact, calculateSurvivalRate } from "$lib/utils";
+
 export const load = async ({ params }) => {
 	const matchData = import(`$lib/server/matches/${params.id}.json`).then(data => data.default).then(data => {
+		const rounds = data.rounds.length
+		console.log(Object.values(data.playerStats))
 		return {
 			...data,
-			playerStats: data.playerStats.sort((a, b) => b.damage_total - a.damage_total)
+			playerStats: data.playerStats.map(playerStat => {
+				const kpr = playerStat.kills_total / rounds
+				const dpr = playerStat.deaths_total / rounds
+				const apr = playerStat.assists_total / rounds
+				const adr = playerStat.damage_total / rounds
+				const deaths = playerStat.deaths_total
+				const survivalRate = (rounds - deaths) / rounds
+				const impact = calculateImpact(playerStat)
+
+				return {
+					...playerStat,
+					rawHLTVRating: { kpr, dpr, apr, impact, adr, survivalRate },
+					hltvRating: calculateHLTVRating(kpr, dpr, apr, impact, adr, survivalRate)
+				}
+			}).sort((a, b) => b.hltvRating - a.hltvRating)
 		}
 	})
 
